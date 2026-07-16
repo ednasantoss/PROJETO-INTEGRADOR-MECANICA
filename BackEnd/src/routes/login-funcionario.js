b4302280f75101a6aa2f3bce5f01575c61885da3
Edna Santos <120690473+ednasantoss@users.noreply.github.com>
2026-07-16 00:23:00 -0300

    Projeto Integrador Mecanica

--- BackEnd/src/routes/login-funcionario.js
+++ BackEnd/src/routes/login-funcionario.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const database = require('../config/db');
 
 router.post('/cadastro', (req, res) => {
